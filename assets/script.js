7c6f5cd01f83b6738f0f1c37413380f93498b9ae
emma-231925 <emma.barnes29@gmail.com>
2022-12-05 13:07:38 +0000

    Updated gitignore, removed DS store and add

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,7 @@ function selectTopic() {
         console.log("Let's study CSS!");
     } else if (randomTopic === 'Git') {
         console.log("Let's study Git!");
-    } else if (randomTopic === 'JavaScript') {
+    } else if (randomTopic === 'JavaScript!') {
         console.log("Let's study JavaScript");
     } else {
         console.log('Please try again!');
